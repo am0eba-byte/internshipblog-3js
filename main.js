de7e51aa1e0cc38e00eb1edae254e35ec29015b4
Mia Borgia <70165410+am0eba-byte@users.noreply.github.com>
2021-11-27 12:30:47 -0500

    Updates

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // import './style.css';
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
 
 // Setup
 
